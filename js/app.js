86ac1586ea744906ab1b2ca3c40236d5e2281936
weter120 <vladzborovsky@gmail.com>
2019-11-01 15:53:38 +0300

    initial commit

---
+++ js/app.js
@@ -0,0 +1,43 @@
+const fill = document.querySelector('.fill');
+const empties = document.querySelectorAll('.empty');
+
+// Fill listeners
+
+fill.addEventListener('dragstart', dragStart);
+fill.addEventListener('dragend',dragEnd);
+
+
+
+for (const empty of empties){
+    empty.addEventListener('dragover', dragOver);
+    empty.addEventListener('dragenter', dragEnter);
+    empty.addEventListener('dragleave', dragLeave);
+    empty.addEventListener('drop', dragDrop);
+}
+// Drag Functions
+
+function dragStart(){
+    // console.log('start');
+    this.className += ' hold';
+    setTimeout(()=>(this.className = 'invisible'), 0);
+}
+
+function dragEnd(){
+    console.log('end');
+    this.className = 'fill';
+}
+
+function dragOver(e){
+    e.preventDefault();
+}
+function dragEnter(){
+    e.preventDefault();
+    this.className += 'hovered';
+}
+function dragLeave(){
+    this.className = 'empty';    
+}
+function dragDrop(){
+    this.className = 'empty';
+    this.append(fill);
+}
